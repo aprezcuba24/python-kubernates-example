f062d8198e5b89acb20c12cdaa9e26adfc95c3e1
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2023-06-13 19:28:55 -0400

    Configure mongo db

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/dev"
+mongo = PyMongo(app)
+db = mongo.db
 
 
 @app.route("/")
-def hello():
-    return "Hello world"
+def get_all_tasks():
+    tasks = db.task.find()
+    data = []
+    for task in tasks:
+        item = {"id": str(task["_id"]), "task": task["task"]}
+        data.append(item)
+    return jsonify(data=data)
+
+
+@app.route("/", methods=["POST"])
+def create_task():
+    data = request.get_json(force=True)
+    db.task.insert_one({"task": data["task"]})
+    return jsonify(message="Task saved successfully!")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
